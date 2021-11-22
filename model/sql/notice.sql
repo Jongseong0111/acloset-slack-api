2a555f75ba79798d6e5ae05236d7641213045f01
임종성 <qkqndudnxld@gmail.com>
2021-11-22 20:55:35 +0900

    sqlc generate

--- model/sql/notice.sql
+++ model/sql/notice.sql
@@ -1,59 +1,50 @@
 -- name: CountAllUsers :one
-
 SELECT count(nua.id)
 FROM nc_usr_account nua
 WHERE nua.del_flag=false;
 
 -- name: CountRecentRegisteredUsers :one
-
 SELECT count(nua.id)
 FROM nc_usr_account nua
 WHERE nua.del_flag=false AND nua.create_dt>(now() - interval '7 day');
 
 -- name: CountAllClothes :one
-
 SELECT count(nco.id)
 FROM nc_core_closet nco
 WHERE nco.del_flag=false AND nco.use_flag=true;
 
--- name: CountRecentRegisteredClothes
-
+-- name: CountRecentRegisteredClothes :one
 SELECT count(nco.id)
 FROM nc_core_closet nco
 WHERE nco.del_flag=false AND nco.use_flag=true AND nco.create_dt>(now() - interval '7 day');
 
--- name: CountAllOutfits
-
+-- name: CountAllOutfits :one
 SELECT count(nco.id)
 FROM nc_core_outfit nco
 WHERE nco.del_flag=false AND nco.use_flag=true;
 
--- name: CountRecentRegisteredOutfits
+-- name: CountRecentRegisteredOutfits :one
 
 SELECT count(nco.id)
 FROM nc_core_outfit nco
 WHERE nco.del_flag=false AND nco.use_flag=true AND nco.create_dt>(now() - interval '7 day');
 
--- name: CountAllPosts
-
+-- name: CountAllPosts :one
 SELECT count(post.post_id)
 FROM post
 WHERE post.is_delete=false;
 
--- name: CountRecentRegisteredPosts
-
+-- name: CountRecentRegisteredPosts :one
 SELECT count(post.post_id)
 FROM post
 WHERE post.is_delete=false AND post.create_date>(now() - interval '7 day');
 
--- name: CountAllSchedules
-
+-- name: CountAllSchedules :one
 SELECT count(nus.id)
 FROM nc_usr_schedule nus
 WHERE nus.del_flag=false AND nus.use_flag=true;
 
--- name: CountAllSchedules
-
+-- name: CountRecentSchedules :one
 SELECT count(nus.id)
 FROM nc_usr_schedule nus
 WHERE nus.del_flag=false AND nus.use_flag=true AND nus.create_dt>(now() - interval '7 day');
